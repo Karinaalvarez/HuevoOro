d9a2ad42786b1398a3e68cca41a876ce20279740
KARINA LIZETH ALVAREZ ANDINO <klalvarez@uth.hn>
2025-11-10 21:32:26 -0600

    git commit -m "Fix vite config for GitHub Pages 3"

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    base: "/HuevoOro/",
   plugins: [react()],
+   base: "/HuevoOro/",
   server: {
     port: 3000,
     proxy: {
